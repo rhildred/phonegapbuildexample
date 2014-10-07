43b91027a05aedc299cd8d794f83abbfcb4be2eb
rhildred <rhildred@gmail.com>
2014-10-07 12:29:58 -0400

    getting working on github.io

--- jssrc/chat.js
+++
@@ -1,9 +0,0 @@
-var jQuery=require('js-toolbox')._jQuery;
-
-module.exports.chat = function(socket){
-	socket.emit('message', {message: "Welcome to this chat"});
-	socket.on('message', jQuery.proxy(function(oData){
-		console.log("echo message");
-		this.sockets.emit("message", oData);
-	}, this));
-};
